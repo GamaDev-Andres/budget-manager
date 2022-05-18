855c0067df265c2680436fbdc40acbd533b21efc
Andres Gama <gama_pipe@hotmail.com>
2022-05-18 18:34:00 -0500

    create function for create jwt

---
+++ backend/helpers/jwt.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+
+const generarJWT = (id, name) => {
+  const payload = { id, name }
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, process.env.FRASE_SECRETA, { expiresIn: 86400 }, (err, token) => {
+      if (err) {
+        console.log(err);
+        reject("Error generando token")
+      }
+      resolve(token)
+    })
+  })
+}
+
+export default generarJWT
