5d2f116f491428b8c0729ba2d3befba49d84264a
Andres Gama <gama_pipe@hotmail.com>
2022-05-19 00:07:18 -0500

    create controller update transaction

--- backend/controllers/transaction.js
+++ backend/controllers/transaction.js
@@ -45,3 +45,31 @@ export const deleteTransaction = async (req, res) => {
     })
   }
 }
+export const updateTransaction = async (req, res) => {
+
+  const { id } = req.params
+  const { user_id } = req.user
+  // const { } = req.body
+  try {
+
+    const transaction = await Transaction.findOne({ where: { transaction_id: id } })
+    if (transaction.user_id !== user_id) {
+      return res.status(403).json({
+        ok: false,
+        errors: [{ msg: "No tiene permiso de actualizar esta transaccion." }]
+      })
+    }
+    transaction.set({ ...req.body })
+    await transaction.save()
+    res.json({
+      ok: true,
+      transaction
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      errors: [{ msg: "Server error" }]
+    })
+  }
+}
--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { createTransaction, deleteTransaction } from '../controllers/transaction.js';
+import { createTransaction, deleteTransaction, updateTransaction } from '../controllers/transaction.js';
 import { validateFields } from '../middlewares/validate-fields.js';
 import { validateJWT } from '../middlewares/validateJWT.js';
 const router = Router()
@@ -15,5 +15,12 @@ router.post("/", [
   check("type", "el tipo es incorrecto").custom(type => type === "ingreso" || type === "egreso"),
 ], validateFields, createTransaction)
 router.delete("/:id", deleteTransaction)
+router.put("/:id", [
+  check("value", "El valor debe ser tipo numero").optional().custom(value => typeof value === "number"),
+  check("value", "El valor debe ser mayor a 0").optional().custom(value => value > 0),
+  check("concept", "el concepto es obligatorio").optional().isString().notEmpty(),
+  check("category", "la categoria debe ser numerica").optional().isNumeric(),
+  check("type", "el tipo no se puede actualizar").isEmpty(),
+], validateFields, updateTransaction)
 
 export default router
