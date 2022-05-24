6486c738756dbb3f19fb4d3d75f2aec20bea3e56
Andres Gama <gama_pipe@hotmail.com>
2022-05-24 17:46:57 -0500

    add estado de form a Register

--- frontend/src/pages/account/register/Register.jsx
+++ frontend/src/pages/account/register/Register.jsx
@@ -1,45 +1,73 @@
 import { Link } from 'react-router-dom'
+import useForm from '../../../hooks/useForm'
 
 const Register = () => {
+  const { dataForm, handleChange } = useForm({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    name: ''
+  })
+  const { email, password, confirmPassword, name } = dataForm
+
   const handleSubmit = (e) => {
     e.preventDefault()
   }
   return (
-    <form className="flex flex-col gap-4 w-full" autoComplete="off">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 w-full"
+      autoComplete="off"
+    >
       <label className="text-gray-600 text-sm">
         email:
         <input
+          required
           className="outline-none py-1 px-2 rounded-md focus:border focus:border-gray-400 border border-gray-300 w-full"
           placeholder="email"
           type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
         />
       </label>
       <label className="text-gray-600 text-sm">
         nombre:
         <input
+          required
           className="outline-none py-1 px-2 rounded-md focus:border focus:border-gray-400 border border-gray-300 w-full"
           placeholder="nombre"
           type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
         />
       </label>
       <label className="text-gray-600 text-sm">
         contraseña:
         <input
+          required
           className="outline-none py-1 px-2 rounded-md focus:border focus:border-gray-400 border border-gray-300 w-full"
           placeholder="password"
           type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
         />
       </label>
       <label className="text-gray-600 text-sm">
         confirmar contraseña:
         <input
+          required
           className="outline-none py-1 px-2 rounded-md focus:border focus:border-gray-400 border border-gray-300 w-full"
           placeholder="confirmar contraseña"
           type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
         />
       </label>
       <button
-        onClick={handleSubmit}
         className="w-full p-1 bg-blue-500 border border-blue-600 rounded-md active:bg-blue-600
         transition-colors duration-300 text-white"
       >
