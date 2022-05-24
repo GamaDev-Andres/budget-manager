77090b97d2073d39311f64e16e6f671972f29262
Andres Gama <gama_pipe@hotmail.com>
2022-05-24 18:35:16 -0500

    fix title account

--- frontend/src/pages/account/Account.jsx
+++ frontend/src/pages/account/Account.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 const Account = () => {
+  const { pathname } = useLocation()
   return (
     <div className="flex items-center justify-center min-h-screen w-full bg-white">
       <div className="flex flex-col items-center justify-center max-w-[350px] w-full rounded-2xl sm:border sm:border-gray-300 py-4 px-8">
-        <h1 className="text-black font-bold text-center text-3xl">Login</h1>
-        <div className="flex items-center justify-center w-full py-2"></div>
+        <h1 className="text-black font-bold text-center text-3xl my-1">
+          {pathname.includes('register') ? 'Registrarse' : 'Login'}
+        </h1>
         <div className="w-full flex items-center justify-center">
           <Outlet />
         </div>
