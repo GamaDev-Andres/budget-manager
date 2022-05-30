f5616735320f37636ccfd2d4b705aa94f25e83ee
Andres Gama <gama_pipe@hotmail.com>
2022-05-29 23:34:04 -0500

    cambiando conexion mysql

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -2,7 +2,4 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const sequelize = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASSWORD_DB || "", {
-  host: process.env.HOST_DB,
-  dialect: 'mysql'
-});
+export const sequelize = new Sequelize(process.env.URL_DB);
